08eb90986e71da33ddd758070e324213ecc23681
Francesco Bianco <info.francescobianco@gmail.com>
2021-11-22 18:44:26 +0100

    Create webrequest-toggle-button.php

---
+++ webrequest-toggle-button.php
@@ -0,0 +1,22 @@
+<?php
+
+// Capture inputs
+$event = $_REQUEST["ifttt_event"];
+$webhooksKey = $_REQUEST["ifttt_webhooks_key"];
+$value1 = $_REQUEST["value1"];
+$value2 = $_REQUEST["value2"];
+$value3 = $_REQUEST["value3"];
+
+// Prepare webhook payload
+$url = "https://maker.ifttt.com/trigger/{$event}/with/key/{$webhooksKey}";
+$body = "value1={$value1}&value2={$value2}&value3={$value3}";
+
+// Prepare curl request
+$request = curl_init($url);
+curl_setopt($request, CURLOPT_POST, 1);
+curl_setopt($request, CURLOPT_POSTFIELDS, $body);
+
+// Execute and capture response
+$response = curl_exec($ch);
+curl_close($ch);
+
